9211bd7b024b5b3084dec728b831a3508e9d4884
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-08-30 23:44:26 -0500

    moved ajax request to posts component

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 export default class Posts extends Component {
+
+  $.ajax({
+    type: 'GET',
+    url: WPsettings.root + 'wp/v2/posts',
+    success: function(response) {
+      postArray.push(response.data);
+    }
+  });
+  
   render() {
     return (
       <div>
