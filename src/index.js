4063e409fcd6c2e661caa1c35478645b58ea3985
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-10-03 13:09:54 -0500

    started updating components and adding routes

--- src/index.js
+++ src/index.js
@@ -1,46 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 
-import SiteHeader from './components/header';
-import Posts from './components/posts';
-import SiteFooter from './components/footer';
-import postsAPI from './components/postsAPI';
-
-class App extends Component {
-
-    constructor(props) {
-      super(props);
-
-      this.state = {
-        title: "The OCD Coder",
-        subtitle: "obsessed with code",
-        copyright: "Simple Digital Solutions 2017",
-        postArray: null
-      };
-    }
-
-    // componentDidMount() {
-    //   postsApi.requestPost().then(data => {
-    //     this.setState({postArray: data})
-    //   });
-    // }
-
-  render() {
-    return
-      (
-        <main>
-          <SiteHeader
-            title={state.title}
-            subtitle={state.subtitle}
-          />
-
-          <SiteFooter
-            copyright={state.copyright}
-          />
-        </main>
-      );
-  }
-}
+import App from './components/App'
 
 render (
   <App />,
