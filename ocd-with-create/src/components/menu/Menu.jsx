6d53ac278bdb90b294a13bba5097a6d60b6c4dba
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-10-06 14:49:41 -0500

    added redux, router, and related dependencies

--- ocd-with-create/src/components/menu/Menu.jsx
+++ ocd-with-create/src/components/menu/Menu.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 
 export default class Menu extends Component {
-  <div>
-    <h1>Menu</h1>
-  </div>
+  render() {
+    return (
+      <div>
+        <h1>Menu</h1>
+      </div>
+    )
+  }
 }
