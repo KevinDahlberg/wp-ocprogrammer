3ca6ad791cefa6ec33a783f9f76f0d2b13086908
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-10-16 19:48:05 -0500

    theme working on dev environment

--- functions.php
+++ functions.php
@@ -6,11 +6,11 @@
 function theme_scripts() {
   wp_enqueue_style ('stylesheet', get_stylesheet_uri() );
 
-  //adds the JS file that queries the DB
-  //wp_enqueue_script ('queries', get_template_directory_uri() . '/src/queries.js', array('jquery'), 1.0, false);
+  wp_enqueue_style ('built_css', get_template_directory_uri() . '/static/css/main.562f8556.css');
+
 
   //adds the compiled react file
-  //wp_enqueue_script ('bundle', get_template_directory_uri() . '/assets/bundle.js', array(), 1.0, true);
+  wp_enqueue_script ('built_js', get_template_directory_uri() . '/static/js/main.073804b7.js', array(), 1.0, true);
 
   //makes various wordpress settings available for use in the JS
   wp_localize_script('queries', 'WPsettings', array(
